a46eebe069bc930c156a5a71d5d024111965c30c
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2023-09-26 04:35:35 +0530

    Add files via upload

---
+++ devops/chef/master/sep2020/QTHMS/chef-repo/cookbooks/qthms-app/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: qthms-app
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+include_recipe 'qthms-app::utils'
+include_recipe 'qthms-app::tomcat'
---
+++ devops/chef/master/sep2020/QTHMS/chef-repo/cookbooks/qthms-db/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: qthms-db
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+include_recipe 'qthms-db::pginstall'
