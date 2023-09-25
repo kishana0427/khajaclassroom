e4ff4fd87576a2eb604db8b15d42d8bce55d5217
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2023-09-26 02:54:09 +0530

    Add files via upload

---
+++ azure/developer/Mar21/Aspnet/SecureWeb/SecureWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecureWeb.Startup))]
+namespace SecureWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
